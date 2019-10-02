7911f7fc9cea8a2d3d4ad99638626c2a436d7a3f
Luann Luna <luann.marques@gmail.com>
2019-10-01 22:13:26 -0300

    App Dicee

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Luann Marques Luna on 01/10/19.
+//  Copyright © 2019 Luann Luna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateDiceImages()
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6)) + 1
+        randomDiceIndex2 = Int(arc4random_uniform(6)) + 1
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+}
+
