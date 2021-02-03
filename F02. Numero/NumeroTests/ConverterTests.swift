fbcd7a22490350c3b4a744d07d3a5d2170156a07
Luann Luna <luann.luna@dextra-sw.com>
2021-02-03 14:57:37 -0300

    Init TDD implementation

---
+++ F02. Numero/NumeroTests/ConverterTests.swift
@@ -0,0 +1,83 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Luann Marques Luna on 03/02/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+
+    let converter = Converter()
+    
+    func testConversionForOne() {
+        let result = converter.convert(1)
+        
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    }
+    
+    func testConversionForTwo() {
+        let result = converter.convert(2)
+        
+        XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+    }
+    
+    func testConversionForFive() {
+        let result = converter.convert(5)
+        
+        XCTAssertEqual(result, "V", "Conversion for 5 is incorrect")
+    }
+    
+    func testConversionForSix() {
+        let result = converter.convert(6)
+        
+        XCTAssertEqual(result, "VI", "Conversion for 6 is incorrect")
+    }
+    
+    func testConversionForTen() {
+        let result = converter.convert(10)
+        
+        XCTAssertEqual(result, "X", "Conversion for 10 is incorrect")
+    }
+    
+    func testConversionForTwenty() {
+        let result = converter.convert(20)
+        
+        XCTAssertEqual(result, "XX", "Conversion for 20 is incorrect")
+    }
+    
+    func testConversionForFour() {
+        let result = converter.convert(4)
+        
+        XCTAssertEqual(result, "IV", "Conversion for 4 is incorrect")
+    }
+    
+    func testConversionForNine() {
+        let result = converter.convert(9)
+        
+        XCTAssertEqual(result, "IX", "Conversion for 9 is incorrect")
+    }
+    
+    func testConversionForTwentyEight() {
+        let result = converter.convert(28)
+        
+        XCTAssertEqual(result, "XXVIII", "Conversion for 25 is incorrect")
+    }
+    
+    func testConverstionForZero() {
+      let result = converter.convert(0)
+        
+      XCTAssertEqual(result, "", "Conversion for 0 is incorrect")
+    }
+    
+    func testConverstionFor3999() {
+      let result = converter.convert(3999)
+        
+      XCTAssertEqual(result, "MMMCMXCIX", "Conversion for 3999 is incorrect")
+    }
+
+
+}
